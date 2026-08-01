c80c3c4925ab9a0023d1e2b545654fee5d6b62a4
Sarthak <sarthakshete090909@gmail.com>
2026-08-01 23:09:55 +0530

    Added p1 and p2 in package_program folder

---
+++ Package1.java
@@ -0,0 +1,20 @@
+package Raj;
+
+public class Package1
+{
+	private int x;
+	private int y;
+	
+	public void setData(int p, int q)
+	{
+		x=p;
+		y=q;
+	}
+	
+	public void display()
+	{
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
+
---
+++ Package2.java
@@ -0,0 +1,13 @@
+package Simran;
+import Raj.Package1;
+
+class Package2
+{
+	public static void main(String args[])
+	{
+		Package1 p1=new Package1();
+		p1.setData(45,18);
+		p1.display();
+		
+	}
+}
